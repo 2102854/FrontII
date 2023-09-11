59a8cd590faa8c11f0b046625264557cd5fe5fed
Eliézer Caixeta <2102854@aluno.univesp.br>
2023-09-11 18:53:19 -0300

    otimização e ajustes nos metodos HttpClient e HttpHeader

--- src/app/modules/components/cidades/cidades.service.ts
+++ src/app/modules/components/cidades/cidades.service.ts
@@ -15,40 +15,67 @@ import { Cidade } from './cidades.model';
 	export class CidadesService {
 
   		baseUrl = `${environment.baseUrl}/cidades`
-
- 		token = localStorage.getItem('@sisGerTransPac-t')
-
-		headers = new HttpHeaders({  
-			"x-access-token": String(this.token),
-			"Access-Control-Allow-Credentials": "true",
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Methods": "PUT,GET,POST,DELETE",
-			"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
-			"Content-Type" : "application/json",
-			"Accept" : "application/json"
-		} ) 
-
   		constructor(private httpCliente: HttpClient, private router: Router, private loginService: LoginService) { }
 
 		ngOnInit(): void {		
 		}
     
 		read(): Observable<Cidade[]> {   
-			return this.httpCliente.get<Cidade[]>(this.baseUrl,{ headers: this.headers})
+			let token = localStorage.getItem('@sisGerTransPac-t')
+			let headers = new HttpHeaders({  
+				"x-access-token": String(token),
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			})				
+			return this.httpCliente.get<Cidade[]>(this.baseUrl,{ headers: headers})
 		}	
 
 		readById(id: string): Observable<Cidade> {
+			let token = localStorage.getItem('@sisGerTransPac-t')
+			let headers = new HttpHeaders({  
+				"x-access-token": String(token),
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			})				
 			const url = `${this.baseUrl}/${id}`
-			return this.httpCliente.get<Cidade>(url,{ headers: this.headers})
+			return this.httpCliente.get<Cidade>(url,{ headers: headers})
 		}
 		
 		create(cidade: Cidade): Observable<Cidade> {
+			let token = localStorage.getItem('@sisGerTransPac-t')
+			let headers = new HttpHeaders({  
+				"x-access-token": String(token),
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			})				
 			const url = `${this.baseUrl}/add`
-			return this.httpCliente.post<Cidade>(url, cidade, { headers: this.headers})
+			return this.httpCliente.post<Cidade>(url, cidade, { headers: headers})
 		}
 		
 		update(cidade: Cidade): Observable<Cidade> {
+			let token = localStorage.getItem('@sisGerTransPac-t')
+			let headers = new HttpHeaders({  
+				"x-access-token": String(token),
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			})				
 			const url = `${this.baseUrl}/update/${cidade.cidade_id}`
-			return this.httpCliente.put<Cidade>(url, cidade, { headers: this.headers})
+			return this.httpCliente.put<Cidade>(url, cidade, { headers: headers})
 		}		
 }
--- src/app/modules/components/estados/estados.service.ts
+++ src/app/modules/components/estados/estados.service.ts
@@ -15,40 +15,67 @@ import { Estado } from './estados.model';
 	export class EstadosService {
 
   		baseUrl = `${environment.baseUrl}/estados`
-
- 		token = localStorage.getItem('@sisGerTransPac-t')
-
-		headers = new HttpHeaders({  
-			"x-access-token": String(this.token),
-			"Access-Control-Allow-Credentials": "true",
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Methods": "PUT,GET,POST,DELETE",
-			"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
-			"Content-Type" : "application/json",
-			"Accept" : "application/json"
-		} ) 
-
   		constructor(private httpCliente: HttpClient, private router: Router, private loginService: LoginService) { }
 
 		ngOnInit(): void {		
 		}
     
-		read(): Observable<Estado[]> {   
-			return this.httpCliente.get<Estado[]>(this.baseUrl,{ headers: this.headers})
+		read(): Observable<Estado[]> {   			
+			let token = localStorage.getItem('@sisGerTransPac-t')
+			let headers = new HttpHeaders({  
+				"x-access-token": String(token),
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			})	
+			return this.httpCliente.get<Estado[]>(this.baseUrl,{ headers: headers})
 		}	
 
 		readById(id: string): Observable<Estado> {
+			let token = localStorage.getItem('@sisGerTransPac-t')
+			let headers = new HttpHeaders({  
+				"x-access-token": String(token),
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			})				
 			const url = `${this.baseUrl}/${id}`
-			return this.httpCliente.get<Estado>(url,{ headers: this.headers})
+			return this.httpCliente.get<Estado>(url,{ headers: headers})
 		}
 		
 		create(estado: Estado): Observable<Estado> {
+			let token = localStorage.getItem('@sisGerTransPac-t')
+			let headers = new HttpHeaders({  
+				"x-access-token": String(token),
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			})				
 			const url = `${this.baseUrl}/add`
-			return this.httpCliente.post<Estado>(url, estado, { headers: this.headers})
+			return this.httpCliente.post<Estado>(url, estado, { headers: headers})
 		}
 		
 		update(estado: Estado): Observable<Estado> {
+			let token = localStorage.getItem('@sisGerTransPac-t')
+			let headers = new HttpHeaders({  
+				"x-access-token": String(token),
+				"Access-Control-Allow-Credentials": "true",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET",
+				"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+				"Content-Type" : "application/json",
+				"Accept" : "application/json"
+			})				
 			const url = `${this.baseUrl}/update/${estado.estado_id}`
-			return this.httpCliente.put<Estado>(url, estado, { headers: this.headers})
+			return this.httpCliente.put<Estado>(url, estado, { headers:headers})
 		}		
 }
